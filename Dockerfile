8e8aef16ed6201d4e755cda2cfaac40e3d2c036a
Glauber Camargo Campos Rocha <gccr92@gmail.com>
2020-01-30 10:14:38 -0300

    created endpoints for GET and SET redis

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /var/www/app
 
 COPY package.json ./
 
+# Remove cert validation because of "self signed certificate" error behind company proxy
+RUN yarn config set strict-ssl false
 RUN yarn
 
 COPY . .
--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const redisClient = require("./redis-client");
+
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  return res.send("Hello World");
+  return res.send(
+    "POST /redis\n\tbody: {key:key, value:value}\n\nGET /store/:redis"
+  );
+});
+
+app.post("/redis", async (req, res) => {
+  try {
+    const { key, value } = req.body;
+    await redisClient.setAsync(key, JSON.stringify(value));
+    return res.status(201).send("Added successfully");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+app.get("/redis/:key", async (req, res) => {
+  try {
+    const { key } = req.params;
+    const rawData = await redisClient.getAsync(key);
+    return res.json(JSON.parse(rawData));
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 });
 
 const PORT = process.env.PORT || 3000;
